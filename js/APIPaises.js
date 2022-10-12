af9544cb8ba9b46535aa61a631fdb7343f2074e9
Ingluissolis <edusolis@yahoo.com.ar>
2022-10-12 16:08:11 -0300

    Se agrega try-catch en APIs

--- js/APIPaises.js
+++ js/APIPaises.js
@@ -1,37 +1,37 @@
 const grupoTodos = []
 
-const API_PAISES_fetch = () => {
-  try {
-    fetch('/BBDD/paises.json').then((response) => {
-      response.json().then((pais) => {
-        grupoTodos.push(...pais);
-        console.log("Respuesta OK base de datos Local");
-        /*Visualizacion de plantilla Grupos en MOD luego de respuesta base de datos*/
-        maquetarGrupoDOM();
-        EscucharClickGrupo();
-      })
-  })
-  } catch (error) {
-    console.error("Error: ", error);
-  }
-}
-
-API_PAISES_fetch()
-
-// /*ASINC - AWAIT */
-// const API_PAISES = async () => {
+// const API_PAISES_fetch = () => {
 //   try {
-//     const respuesta = await fetch("/BBDD/paises.json");
-//     const pais = await respuesta.json();
-//     grupoTodos.push(...pais);
-//     console.log("Respuesta base de datos Local");
-//     /*Visualizacion de plantilla Grupos en MOD luego de respuesta base de datos*/
-//     maquetarGrupoDOM()
-//     EscucharClickGrupo()
+//     fetch('/BBDD/paises.json').then((response) => {
+//       response.json().then((pais) => {
+//         grupoTodos.push(...pais);
+//         console.log("Respuesta OK base de datos Local");
+//         /*Visualizacion de plantilla Grupos en MOD luego de respuesta base de datos*/
+//         maquetarGrupoDOM();
+//         EscucharClickGrupo();
+//       })
+//   })
 //   } catch (error) {
 //     console.error("Error: ", error);
 //   }
-  
-// };
+// }
 
-// API_PAISES()
+// API_PAISES_fetch()
+
+/*ASINC - AWAIT */
+const API_PAISES = async () => {
+  try {
+    const respuesta = await fetch("/BBDD/paises.json");
+    const pais = await respuesta.json();
+    grupoTodos.push(...pais);
+    console.log("Respuesta base de datos Local");
+    /*Visualizacion de plantilla Grupos en MOD luego de respuesta base de datos*/
+    maquetarGrupoDOM();
+    EscucharClickGrupo();
+  } catch (error) {
+    console.error("Error: ", error);
+  }
+  
+};
+
+API_PAISES()
